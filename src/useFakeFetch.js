58b554d92977591ae7ee4e70cf0b4059df725e03
Biraj Rajbhandari <birajr0@gmail.com>
2021-03-14 15:28:45 +0545

    Changes title and adds fetch by id

--- src/useFakeFetch.js
+++ src/useFakeFetch.js
@@ -4,6 +4,14 @@ const useFakeFetch = (url) => {
   if (url === "http://localhost:8000/blogs")
     return { data: fakeData, isLoading: false, error: null };
 
+  if (url.startsWith("http://localhost:8000/blogs/")) {
+    const blogId = url.slice(28);
+    const blog = fakeData.find((b) => b.id.toString() === blogId);
+    if (blog === undefined) {
+      return { data: null, isLoading: false, error: "Not found" };
+    }
+    return { data: blog, isLoading: false, error: null };
+  }
   return { data: null, isLoading: false, error: "Unknown url" };
 };
 
